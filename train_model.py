89eebc0402caa15505ea6c7590675e64b0505183
Kathry1 <kthfuchs@web.de>
2025-04-19 11:50:55 +0200

    Replace broken model.pkl with valid pickle

--- train_model.py
+++ train_model.py
@@ -1,10 +1,12 @@
-import pickle
 from sklearn.linear_model import LogisticRegression
 from sklearn.datasets import load_iris
+import pickle
 
+# Modell trainieren
 X, y = load_iris(return_X_y=True)
 model = LogisticRegression(max_iter=200)
 model.fit(X, y)
 
+# Modell speichern
 with open("model.pkl", "wb") as f:
     pickle.dump(model, f)
